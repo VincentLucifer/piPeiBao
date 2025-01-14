da5971a74e0c217e85d82e2e3305d24bb524495c
wenzifeng <2165800367@qq.com>
2025-01-14 16:05:29 +0800

    Final commit

---
+++ src/main/java/com/yupi/yupao/mapper/TagMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.yupao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yupao.model.domain.Tag;
+
+
+/**
+* @author LENOVO
+* @description 针对表【tag(标签)】的数据库操作Mapper
+* @createDate 2025-01-08 14:08:32
+* @Entity generator.domain.Tag
+*/
+public interface TagMapper extends BaseMapper<Tag> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/yupao/service/TagService.java
@@ -0,0 +1,13 @@
+package com.yupi.yupao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.yupao.model.domain.Tag;
+
+/**
+* @author LENOVO
+* @description 针对表【tag(标签)】的数据库操作Service
+* @createDate 2025-01-08 14:08:32
+*/
+public interface TagService extends IService<Tag> {
+
+}
